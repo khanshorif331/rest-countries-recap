37a2e3c5d2f6fc27ed79aa39177c64e308f93f6a
khanshorif331 <khanshorif331@gmail.com>
2022-03-15 00:26:37 +0600

    more data loads,flags added

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-console.log('i love you');
 const loadCountries = ()=>{
     fetch('https://restcountries.com/v3.1/all')
     .then(res =>res.json())
     .then(data => displayCountries(data))
 }
 
-const displayCountries = (country) =>{
-    console.log(country);
+const displayCountries = countries =>{
+    console.log(countries[0]);
+    const countriesHtml = countries.map(country=>getCountryHtml(country))
+    const container = document.getElementById('countries')
+    container.innerHTML = countriesHtml.join(' ')
+}
+
+
+const getCountryHtml = country =>{
+    return `
+        <div>
+            <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}">
+        </div>
+    `
 }
 
 
