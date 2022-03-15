23f17e5347ffc026c8222a05bf6c79d114d422ee
khanshorif331 <khanshorif331@gmail.com>
2022-03-15 19:18:31 +0600

    done practising this

--- app.js
+++ app.js
@@ -28,29 +28,55 @@
 // pracricing
 
 
-const loadCountries =() =>{
+// const loadCountries =() =>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res => res.json())
+//     .then(data =>displayCountries(data))
+// }
+
+// const displayCountries = countries =>{
+//     // console.log(countries);
+//     const countriesHtml = countries.map(country=>getCountryHtml(country))
+//     const container =document.getElementById('countries')
+//     container.innerHTML = countriesHtml.join(' ')
+//     // console.log(countriesHtml);
+// }
+
+// const getCountryHtml=country=>{
+//     return `
+//     <div class= "country">
+//         <h2>${country.name.common}</h2>
+//         <img src = "${country.flags.png}">
+//     </div>
+//     `
+// }
+
+
+
+// loadCountries()
+
+// again practising
+
+const loadCountries = ()=>{
     fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
+    .then(res =>res.json())
     .then(data =>displayCountries(data))
 }
 
 const displayCountries = countries =>{
-    // console.log(countries);
-    const countriesHtml = countries.map(country=>getCountryHtml(country))
-    const container =document.getElementById('countries')
-    container.innerHTML = countriesHtml.join(' ')
-    // console.log(countriesHtml);
+    const countriesHtml=countries.map(country=>getCountryHtml(country))
+    const container = document.getElementById('countries')
+    container.innerHTML=countriesHtml.join(' ')
+
 }
 
-const getCountryHtml=country=>{
+const getCountryHtml =country=>{
     return `
-    <div class= "country">
-        <h2>${country.name.common}</h2>
-        <img src = "${country.flags.png}">
+    <div>
+    <h2>${country.name.common}</h2>
+    <img src="${country.flags.png}">
     </div>
     `
 }
-
-
 
 loadCountries()
